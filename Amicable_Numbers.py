f31471a880080f18a8d56f4fc9583de4962f00d2
suryamjv <84082849+suryamjv@users.noreply.github.com>
2022-10-29 10:00:31 +0530

    def pfs(num):     fs=0     for i in range(1,num):         if num%i==0:             fs+=i     return fs a =int(input()) b =int(input()) if pfs(a)==b and pfs(b)==a:     print("Amicable") else:print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,11 @@
+def pfs(num):
+    fs=0
+    for i in range(1,num):
+        if num%i==0:
+            fs+=i
+    return fs
+a =int(input())
+b =int(input())
+if pfs(a)==b and pfs(b)==a:
+    print("Amicable")
+else:print("Not Amicable")
